960848a9132a0334a36aeffc24b2e41af7949d92
исраел едет <jtad009@gmail.com>
2020-06-19 11:49:07 +0100

    refactors the callout for faq

--- js/faq.js
+++ js/faq.js
@@ -4,35 +4,36 @@ const  show = ()=> {
         item.classList.remove('d-none');
     });
 }
-$(document).ready(function(){
-    $(document).on('keyup', '.input-field', function () {
-        $this = $(this);
-        if ($this.val().length === 0) {
-            $(document).find('.suggestions').hide();
-            show();
-            return false;
-        }
-
-        const titles = document.querySelectorAll(".accordion-toggle");
+const search = ()=>{
+    const titles = document.querySelectorAll(".accordion-toggle");
         const match = [...titles].filter(title => {
             return title.innerHTML.toLowerCase().match($this.val().toLowerCase());
         });
         const notMatch = [...titles].filter(title => {
             return !title.innerHTML.toLowerCase().match($this.val().toLowerCase());
         });
-        // //  
-        // // console.log(data);
-
+       
+        //show those that match the creteria
         match.forEach(item => {
             item.parentElement.parentElement.parentElement.classList.remove('d-none')
             item.parentElement.parentElement.parentElement.classList.add('d-block')
-        })
+        });
+
+        //hide those that do not match the creteria
         notMatch.forEach(item => {
             item.parentElement.parentElement.parentElement.classList.remove('d-block')
             item.parentElement.parentElement.parentElement.classList.add('d-none')
         })
-
-
+}
+$(document).ready(function(){
+    $(document).on('keyup', '.input-field', function () {
+        $this = $(this);
+        if ($this.val().length === 0) {
+            show(); //show all response
+            return false;
+        }
+        search();
+        
     });
 
 })
