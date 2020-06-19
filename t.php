2b111de44fe6536aea2ac8ffea47fa0905d792aa
исраел едет <jtad009@gmail.com>
2020-06-19 10:23:05 +0100

    complete UI for doubble

---
+++ t.php
@@ -0,0 +1,119 @@
+<?php
+
+$welcome  = '
+<p dir="ltr">The dilemma that can come with periods are endless and are different for each person. You can get headaches, mood swings, abdominal pain or loss of appetite amongst others. All these can leave you feeling crappy, less confident and not your usual happy self. It is important to do what makes you happy so that you feel less irritable and more confident during your period.&nbsp; Here are a few tips to help keep your confidence level at 100% on your period days<br></p><ul><li dir="ltr"><p dir="ltr" role="presentation"><span>Stop Guessing</span><span>: Know your period days. Using guesswork to plan for your period may be effective sometimes, but it is definitely the worst way to figure out the day your period is supposed to start. Find an effective period tracker or calendar to help you keep track of your period. Luckily, Diva pads have an effective and easy to use period tracker which you can find on&nbsp; </span><a href="https://divapads.com"><span>https://divapads.com</span></a><span> </span></p><p dir="ltr" role="presentation"></p><p dir="ltr" role="presentation" style="line-height:1.295;margin-left: 36pt;text-align: center;margin-top:0pt;margin-bottom:0pt;"><img src="https://divapads.com/webroot/img/uploads/img1.png" style="width:50%"><span><br></span></p></li></ul><ul><li dir="ltr"><p dir="ltr" role="presentation"><span>Find the perfect product for you</span><span>:&nbsp; Only you can know what makes you feel comfortable. Fortunately, when it comes to periods and pads, Diva pads are the perfect fit. Either you have a heavy flow or light flow, with diva pads, you are comfortable and do not need to worry about leakage. When you are confident in your product choice, that confidence radiates around you through your period days.</span></p><p dir="ltr" role="presentation"><br></p><p dir="ltr" role="presentation" style="line-height:1.295;margin-left: 36pt;text-align: center;margin-top:0pt;margin-bottom:0pt;"><img src="https://divapads.com/webroot/img/uploads/img2.png" style="width:50%"><span><br></span></p></li></ul><p dir="ltr"></p><p dir="ltr"><br></p><ul><li dir="ltr"><p dir="ltr" role="presentation"><span>Wear comfortable clothes</span><span>: Some ladies may prefer darker colours during their period days in case of a leak. Darker colours are better at hiding stains than bright ones.&nbsp;</span></p></li></ul><p dir="ltr"><span>You can decide to do this too as it is better to know that even though there is a leak, your clothes have got your back. Turning around to check on the state of your butt from time to time can drain your confidence level from 100 to 1 in a minute.</span></p><p dir="ltr"><span><br></span></p><p dir="ltr" style="line-height:1.295;margin-left: 36pt;text-align: center;margin-top:0pt;margin-bottom:0pt;"><img src="https://divapads.com/webroot/img/uploads/img3.png" style="width:50%"><span><br></span></p><p dir="ltr"><span>Take a lot of vitamins: </span><span>The challenges that can come with periods are usually worse if you have a vitamin deficiency. Some vitamins like iron, magnesium, vitamin B and zinc can help reduce issues such as fatigue, dizziness, headaches and cramps during your period. They help you feel healthy and strong. We all know that with good health comes great confidence.</span></p><p dir="ltr"><span><br></span></p><p dir="ltr" style="line-height:1.295;margin-left: 36pt;text-align: center;margin-top:0pt;margin-bottom:0pt;"><img src="https://divapads.com/webroot/img/uploads/img4.png" style="width:50%"><br></p><p><b><br><br></b></p><ul><li dir="ltr"><p dir="ltr" role="presentation"><span>Maintain good hygiene: </span><span>Try</span><span> to take a shower or bath at least twice a day. Also, change your pads or tampons regularly. Sometimes, periods can come with a slight smell that is usually stronger when you do not bath and change your sanitary product when necessary. </span><span>Nothing kills confidence during periods than feeling the weight of your soaked pad or having people twist their nose when they come close to you. Have a bath, change your pads, feel fresh and regain your confidence.</span></p></li></ul><p><b><br></b></p><ul><li dir="ltr"><p dir="ltr" role="presentation"><span><b>Rest: </b></span><span>Your body is putting extra work when you are on your period, and lack of sleep can make you cranky and anxious. Take a break and rest a little. You would wake up feeling better, stronger and refreshed.</span></p></li></ul><p dir="ltr"></p><p dir="ltr" style="line-height:1.295;margin-left: 36pt;text-align: center;margin-top:0pt;margin-bottom:0pt;"><img src="https://divapads.com/webroot/img/uploads/img5.png" style="width:50%"><br></p><p dir="ltr"><span>Drink water and exercise: </span><span>Your period can make you feel bloated. Drinking about eight glasses of water a day can help relieve bloating and fatigue. Exercising will help elevate your mood and increase blood circulation. It also helps to reduce cramps and back pain that may come with periods. We all know that the less pain you feel the more confident you become.</span></p><p dir="ltr"><span><br></span><span>Always remember to wear a smile and try to be your normal bubbly self even on your period days. Nobody can guess that you are on your period, so there is no need to feel self-conscious and uncomfortable. Periods are normal and if you follow the steps highlighted above, be sure to go from feeling like this</span></p><p dir="ltr" style="line-height:1.295;margin-left: 36pt;text-align: center;margin-top:0pt;margin-bottom:0pt;"><img src="https://divapads.com/webroot/img/uploads/img7.jpeg" style="width:50%" class="note-float-left"><span><span></span></span></p><p dir="ltr"><span>to feeling like this&nbsp;</span><span>even on your period days.
+
+</span></p><p style="line-height:1.295;margin-left: 36pt;text-align: center;margin-top:0pt;margin-bottom:0pt;"><img src="https://divapads.com/webroot/img/uploads/img7.png" style="width:50%"></p>';
+
+$output = preg_replace('/(<[^(<img)>]+) style=".*?"/i', '$1', $welcome);
+//preg_replace('/(<[^>]+) style=".*?"/i', '$1', $welcome);
+
+
+echo $welcome;
+?>
+
+<div class="accordion" id="accordionExample">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <a class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Doubble 3
+        </a>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+      Monthly contributions are made by you for the first 3 years and your invested amount plus returns are paid to you every month for the next 3 years.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Doubble 5
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+      Monthly contributions are made by you for the first 5 years and your invested amount plus returns are paid to you every month for the next 5 years. 
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Doubble 10
+        </button>
+      </h2>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+      Monthly contributions are made by you for the first 5 years and your invested amount plus returns are paid to you every month for the next 10 years.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingFour">
+      <h2 class="mb-0">
+        <a class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+         Doubble Lumpsum
+        </a>
+      </h2>
+    </div>
+    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+      <div class="card-body">
+      This is a 10-year investment plan. Contribution is made in one bulk amount and you get double your money starting from year 6 to year 10. 
+      </div>
+    </div>
+  </div>
+</div>
+<div class="mb-4 mt-4 " id="accordion" role="tablist" aria-multiselectable="true">
+    <div class="card  border-0 p-0">
+        <div class="card-header" role="tab" id="headingOne">
+            <h5 class="mb-0">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne" class=" details collapsed accordion-toggle">Doubble 3</a>
+            </h5>
+        </div>
+
+        <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne">
+            <div class="card-body details"> Monthly contributions are made by you for the first 3 years and your invested amount plus returns are paid to you every month for the next 3 years. </div>
+        </div>
+    </div>
+    <div class="card   border-0 p-0">
+        <div class="card-header" role="tab" id="heading2">
+            <h5 class="mb-0">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse2" aria-expanded="false" aria-controls="collapse2" class=" details collapsed accordion-toggle">Doubble 5</a>
+            </h5>
+        </div>
+
+        <div id="collapse2" class="collapse" role="tabpanel" aria-labelledby="heading2">
+            <div class="card-body details"> Monthly contributions are made by you for the first 5 years and your invested amount plus returns are paid to you every month for the next 5 years. </div>
+        </div>
+    </div>
+    <div class="card   border-0 p-0">
+        <div class="card-header" role="tab" id="heading3">
+            <h5 class="mb-0">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse3" aria-expanded="false" aria-controls="collapse3" class="details collapsed accordion-toggle">Doubble 10</a>
+            </h5>
+        </div>
+
+        <div id="collapse3" class="collapse" role="tabpanel" aria-labelledby="heading3">
+            <div class="card-body details"> Monthly contributions are made by you for the first 5 years and your invested amount plus returns are paid to you every month for the next 10 years. </div>
+        </div>
+    </div>
+    <div class="card   border-0 p-0">
+        <div class="card-header" role="tab" id="heading4">
+            <h5 class="mb-0">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse4" aria-expanded="false" aria-controls="collapse4" class="details collapsed accordion-toggle">Doubble Lumpsum</a>
+            </h5>
+        </div>
+
+        <div id="collapse4" class="collapse" role="tabpanel" aria-labelledby="heading4">
+            <div class="card-body "> This is a 10-year investment plan. Contribution is made in one bulk amount and you get double your money starting from year 6 to year 10. </div>
+        </div>
+    </div>
+</div>
---
+++ target.js
@@ -0,0 +1,135 @@
+$(document).ready(function(){
+    $(document).on('keyup', '#rewardYear',function(e){
+        calculateDuration()
+    })
+    $(document).on('keyup', '#target',function(e){
+        addCommas($(this));
+        calTarget();
+    })
+})
+
+/*********************
+ * 
+ * 
+ * TARGET CALCULATOR
+ ********************/
+let rewardYear = $(document).find('#rewardYear');
+let durationWarning = $(document).find('.durationWarning');
+let target = $(document).find('#target');
+let rewardDuration = $(document).find('#rewardDuration');
+let effrate = 0;
+let intRate = 0;
+let numPay = 0;
+let xtenor = 0
+let targetCurrency = $(document).find('#targetCurrency');
+let currentName = '';
+let rewardDurationVal;
+
+const calculateDuration = () => {
+    if (parseInt(rewardYear.val(), 10) <= 5 || rewardYear.val().length == 0) {
+        durationWarning.text('Enter a value not less than 6 months');
+    }
+    else if (between(parseInt(rewardYear.val(), 10), 6, 12)) {
+        durationWarning.text(parseInt(rewardYear.val(), 10) + ' months');
+    }
+    else if (between(parseInt(rewardYear.val(), 10), 13, 23)) {
+        durationWarning.text((parseInt(rewardYear.val(), 10) / 12));
+        var n = durationWarning.text()
+        var year = Math.floor(n);
+        var month = Math.round((n % 1) * 12);
+        if (month == 1) { durationWarning.text((year + ' year, ' + month + ' month')); }
+        else if (month > 1) { durationWarning.text((year + ' year, ' + month + ' months')); }
+    }
+    else if (parseInt(rewardYear.val(), 10) >= 24 && parseInt(rewardYear.val(), 10) < 241) {
+        durationWarning.text((parseInt(rewardYear.val(), 10) / 12));
+        var n2 = durationWarning.text();
+        var year2 = Math.floor(n2);
+        var month2 = Math.round((n2 % 1) * 12);
+        if (month2 === 0) { durationWarning.text((year2 + ' years')); }
+        else if (month2 == 1) { durationWarning.text((year2 + ' years, ' + month2 + ' month')); }
+        else if (month2 > 1) { durationWarning.text((year2 + ' years, ' + month2 + ' months')); }
+    }
+    else if (parseInt(rewardYear.val(), 10) >= 241) {
+        durationWarning.text('Must be between 6 months and 20 years');
+    }
+
+}
+function calTarget() {
+const targetAmount = target.val().replace(/,/g,'');
+    //Change Currency
+    if (targetCurrency.val() === "") { currentName = 'Select preferred Currency'; }
+    else if (targetCurrency.val() == 'N') {
+        //Show Month Input
+        rewardDurationVal = (rewardYear.val() / 12);
+       
+        //Naira Interest Rate
+        if (rewardDurationVal < 0.4) { intRate = 0; }
+        else if (between(rewardDurationVal, 0.5, 3.99)) { intRate = 4 / 100; }
+        else if (between(rewardDurationVal, 4.0, 4.99)) { intRate = 4.5 / 100; }
+        else if (between(rewardDurationVal, 5.0, 200.9)) { intRate = 5 / 100; }
+        console.log(rewardDurationVal)
+    }
+    else if (targetCurrency.val() == 'USD') {
+        //Show Month Input
+        rewardDurationVal = (rewardYear.val() / 12);
+
+        //Dollar Interest Rate
+        if (rewardDurationVal < 0.4) { intRate = 0; }
+        else if (between(rewardDurationVal, 0.5, 3.99)) { intRate = 4 / 100; }
+        else if (between(rewardDurationVal, 4.0, 4.99)) { intRate = 4.5 / 100; }
+        else if (between(rewardDurationVal, 5.0, 200.9)) { intRate = 5 / 100; }
+        console.log(rewardDurationVal)
+    }
+
+    //Effective Rate  === r
+    if (rewardDuration.val() === "") { effrate = '0'; }
+    else if (rewardDuration.val() == 12) { effrate = ((intRate * 0.9)) }
+    else if (rewardDuration.val() == 4) { effrate = ((intRate * 0.9)) }
+    else if (rewardDuration.val() == 2) { effrate = ((intRate * 0.9)) }
+    else if (rewardDuration.val() == 1) { effrate = ((intRate * 0.9)) }
+    console.log(effrate)
+    //Number of Payments === n
+    if (rewardDuration.val() === "") { numPay = 0; }
+    else if (rewardDuration.val() == 12) { numPay = parseInt(rewardDuration.val()) }
+    else if (rewardDuration.val() == 4) { numPay = parseInt(rewardDuration.val()) }
+    else if (rewardDuration.val() == 2) { numPay = parseInt(rewardDuration.val()) }
+    else if (rewardDuration.val() == 1) { numPay = parseInt(rewardDuration.val()) }
+
+    //Tenor in Years === t
+    if (rewardDuration.val() === "") { xtenor = 0; }
+    else if (rewardDuration.val() == 12) { xtenor = (rewardDurationVal) }
+    else if (rewardDuration.val() == 4) { xtenor = (rewardDurationVal) }
+    else if (rewardDuration.val() == 2) { xtenor = (rewardDurationVal) }
+    else if (rewardDuration.val() == 1) { xtenor = (rewardDurationVal) }
+
+
+    ////////// Values ///////////
+    let xFactor = numPay * rewardDurationVal;
+    let yFactor = effrate / numPay;
+    let zFactor = 1 + yFactor;
+    let bFactor = Math.pow(zFactor, xFactor);
+    let cFactor = (bFactor - 1) / yFactor;
+
+    const payment = parseInt(targetAmount, 10) / (cFactor * zFactor);
+    const totalpay = xFactor * payment;
+    const gained = parseInt(targetAmount, 10) - totalpay;
+    const total = parseFloat(gained).toFixed(2) + parseFloat(totalpay).toFixed(2);
+    console.log( targetAmount);
+    if(typeof total === 'string' && total != 'NaNNaN' ){
+        
+        document.getElementById('targetReturns').innerHTML = targetCurrency.val() + ' '+addCommas(parseFloat(total).toFixed(2).toString())
+        
+    } else {
+        document.getElementById('targetReturns').innerHTML =  ' 0 ';
+    }
+    
+    
+    
+    // console.log(parseInt(target.val(), 10) );
+    console.log("Payment: ", addCommas(parseFloat(payment).toFixed(2).toString()));
+    console.log("totalPay: ", addCommas(parseFloat(totalpay).toFixed(2).toString()));
+    console.log("gained: ", addCommas(parseFloat(gained).toFixed(2).toString()));
+
+
+}
+
